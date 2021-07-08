9c68e27973504b389e23a6e2c3776e8a26c81c6b
m-kreuzer <kreuzer@pik-potsdam.de>
2021-07-08 18:46:19 +0200

    more sophisticated functions to redirect stderr AND stdout to logfile

--- create_experiment/create_run.py
+++ create_experiment/create_run.py
@@ -13,6 +13,41 @@ import helpers
 import settings 
 
 
+from contextlib import contextmanager
+
+# redirect stderr/stdout functions copied from
+# https://stackoverflow.com/questions/4675728/redirect-stdout-to-a-file-in-python/22434262#22434262
+def fileno(file_or_fd):
+    fd = getattr(file_or_fd, 'fileno', lambda: file_or_fd)()
+    if not isinstance(fd, int):
+        raise ValueError("Expected a file (`.fileno()`) or a file descriptor")
+    return fd
+
+@contextmanager
+def stdout_redirected(to=os.devnull, stdout=None):
+    if stdout is None:
+       stdout = sys.stdout
+
+    stdout_fd = fileno(stdout)
+    # copy stdout_fd before it is overwritten
+    #NOTE: `copied` is inheritable on Windows when duplicating a standard stream
+    with os.fdopen(os.dup(stdout_fd), 'wb') as copied: 
+        stdout.flush()  # flush library buffers that dup2 knows nothing about
+        try:
+            os.dup2(fileno(to), stdout_fd)  # $ exec >&to
+        except ValueError:  # filename
+            with open(to, 'wb') as to_file:
+                os.dup2(to_file.fileno(), stdout_fd)  # $ exec > to
+        try:
+            yield stdout # allow code to be run with the redirected stdout
+        finally:
+            # restore stdout to its previous value
+            #NOTE: dup2 makes stdout_fd inheritable unconditionally
+            stdout.flush()
+            os.dup2(copied.fileno(), stdout_fd)  # $ exec >&copied
+
+def merged_stderr_stdout():  # $ exec 2>&1
+    return stdout_redirected(to=sys.stdout, stdout=sys.stderr)
 
 
 def create_script_from_template(settings, template_file,
@@ -204,11 +239,15 @@ if __name__ == "__main__":
     print("writing script output to '{}'".format(logfile))
     print("  -> check there for possible errors")
     with open(logfile, "w") as f:
-        sys.stdout = f
-        try:
+        #sys.stdout = f
+        #try:
+        #    create_run()
+        #finally:
+        #    sys.stdout = orig_stdout
+        with stdout_redirected(to=f), merged_stderr_stdout():
+        # copied from https://stackoverflow.com/questions/6796492/temporarily-redirect-stdout-stderr
             create_run()
-        finally:
-            sys.stdout = orig_stdout
+
 
     # copy logfile to experiment location
     logfile_dst = os.path.join(settings.experiment_dir, logfile)
