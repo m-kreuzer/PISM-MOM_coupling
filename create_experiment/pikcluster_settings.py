ceceb87335c902c3848a31cc58ca73813cde99c7
Willem Huiskamp <willemhuiskamp88@gmail.com>
2022-09-26 15:35:57 +0200

    Updated for MOM6

--- create_experiment/pikcluster_settings.py
+++ create_experiment/pikcluster_settings.py
@@ -2,26 +2,21 @@
 
 # ----------------------------------- paths ------------------------------------
 # base directory where experiments will be located (submit/input/output files)
-#working_dir = "/p/projects/pism/kreuzer/coupled_PISM_MOM/experiments"
-working_dir = "/p/tmp/kreuzer/coupled_PISM_MOM/experiments"
+working_dir = "/p/projects/climber3/huiskamp/POEM/work/mom6_runs"
 
 # directory with coupling infrastructure template 
 #  -> clone of https://github.com/m-kreuzer/PISM-MOM_coupling.git
-coupl_template_dir = "/p/projects/pism/kreuzer/software/PISM-MOM_coupling"
+coupl_template_dir = "/p/projects/climber3/huiskamp/POEM/work/PISM-MOM_coupling"
 
 ### PISM paths
-#pism_code_dir = "/p/projects/pism/kreuzer/software/pism1.2.1"
-#pism_code_dir = "/p/projects/climber3/kreuzer/PISM/pism1.1"
 pism_code_dir = "/p/projects/pism/kreuzer/software/pism1.1.4_extra-double"
-#pism_code_dir = "/home/reese/pism_code/pism2019/pism"
 pism_input_root_dir = "/p/projects/pism/kreuzer/pism_input_2020"
 
 ### POEM paths
 # POEM project directory with tools like mppnccombine, time_stamp.csh, ...
-#poem_proj_dir = "/p/projects/climber3/kreuzer/POEM/trunk"
 poem_proj_dir = "/p/projects/climber3/kreuzer/POEM/branches/kreuzer_mom5.1"
 # POEM template directory with all namelist & input files
-poem_template_dir = "/p/projects/climber3/kreuzer/POEM/POEM_PISM_coupling_templates/MOM5_SIS-EM3-no_antarctic_river_runoff-basal"
+poem_template_dir = "/p/projects/climber3/huiskamp/POEM/work/MOM6_SIS2_template"
 
 
 
